3775c7364991254652f03d960366c27cc288fc9d
karim <karim.tarek96@yahoo.com>
2024-04-28 14:52:11 +0300

    inital commit

---
+++ lib/screens/meals.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class MealsScreen extends StatelessWidget{
+
+  
+
+  const MealsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
